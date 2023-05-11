5ba935d3e61881f82e6b7a1ad882fc013f94e7a0
Gopi2301 <gopinaths2301@gmail.com>
2023-05-11 21:57:41 +0530

    error recovered

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -4,10 +4,6 @@ import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 
-const handleChange = () => {};
-const handleSubmit = () => {};
-const handleSurpriseMe = () => {};
-const generateImage = () => {};
 const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
@@ -17,6 +13,40 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = () => { };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt
+          }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
