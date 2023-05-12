80189c76bfbd9fb9b0bb525c223a9856719a988b
Gopi2301 <gopinaths2301@gmail.com>
2023-05-12 20:52:05 +0530

    .env to git ignore

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Loader, Card, FormField } from "../components";
+import { useEffect } from "react";
 
 const RenderCards = ({ data, title }) => {
-  if (data.length > 0) {
+  if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
   }
   return (
@@ -13,6 +14,43 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPost, setAllPost] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+
+        })
+        if (response.ok) {
+          const result = await response.json();
+          setAllPost(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchPost()
+  }, []);
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPost.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLocaleLowerCase()));
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  }
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -25,7 +63,14 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName="SearchPost"
+          type="text"
+          name="text"
+          placeholder="Search Posts.."
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
       <div className="mt-10">
         {loading ? (
@@ -42,9 +87,9 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title="No search results found" />
+                <RenderCards data={searchedResults} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No post found" />
+                <RenderCards data={allPost} title="No post found" />
               )}
             </div>
           </>
